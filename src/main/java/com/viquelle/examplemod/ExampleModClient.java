6995704cf1b6d883de45336a380dc5ef6d8cbe1d
vique1206 <vladivanov1206@gmail.com>
2026-01-08 16:07:35 +0300

    PLAYABLE a bit

--- src/main/java/com/viquelle/examplemod/ExampleModClient.java
+++ src/main/java/com/viquelle/examplemod/ExampleModClient.java
@@ -3,12 +3,8 @@ package com.viquelle.examplemod;
 import com.viquelle.examplemod.client.ClientLightManager;
 import com.viquelle.examplemod.client.ClientPayloadHandler;
 import com.viquelle.examplemod.client.HudRenderer;
-import com.viquelle.examplemod.client.light.AreaLight;
 import com.viquelle.examplemod.network.SanityPayload;
-import foundry.veil.api.client.render.VeilRenderSystem;
-import foundry.veil.api.client.render.light.data.AreaLightData;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -16,12 +12,10 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
-import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.level.LevelEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 
 @Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
@@ -61,7 +55,7 @@ public class ExampleModClient {
             ExampleMod.LOGGER.info("Client TICK");
             Minecraft mc = Minecraft.getInstance();
             if (mc.player != null) {
-                ClientLightManager.initPlayerLight(mc.player);
+                ClientLightManager.initPlayerAmbientLight(mc.player);
             }
         });
 
